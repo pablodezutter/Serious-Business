ce7f89c81ecca3bfae425ce069de975174b65b8c
p <dezutterpablo@gmail.com>
2019-04-03 13:39:44 +0200

    made a small change to ease up the exercise

--- myscript.js
+++ myscript.js
@@ -1,3 +1,19 @@
-var datumentijd= new Date(getDay(),getHours(),getMinutes());
+/*Variabel aanmaken voor de dagen.*/
+var dagen =["Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"];
 
-console.log(datumentijd);
+/*Variabel aanmaken voor de uren.*/
+
+
+/*Placing text in the p*/
+/*document.getElementById("text").innerHTML="OPEN";
+document.getElementById("text").innerHTML="CLOSED";*/
+
+/*Get local day and hours*/
+var today= new Date();
+console.log(today);
+
+/*Look if the day and hour/minutes is equal to the given timeframe in the table*/
+
+/*If yes: OPEN*/
+/*If no: CLOSED*/
+/*If sunday: always CLOSED*/
