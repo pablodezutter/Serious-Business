b01f7dfc628df20111afc914f22635e9be331e73
p <dezutterpablo@gmail.com>
2019-04-03 15:09:28 +0200

    al enkele variabelen gemaakt

--- myscript.js
+++ myscript.js
@@ -2,17 +2,31 @@
 var dagen =["Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"];
 
 /*Variabel aanmaken voor de uren.*/
+var uren=[]
 
 
 /*Placing text in the p*/
-/*document.getElementById("text").innerHTML="OPEN";
-document.getElementById("text").innerHTML="CLOSED";*/
+var getText = document.getElementById("text").write="OPEN";
+/*document.getElementById("text").innerHTML="CLOSED";*/
 
-/*Get local day and hours*/
-var today= new Date();
-console.log(today);
+/*Get local day and hours when page loaded*/
+var today= new Date(day, hours, minutes);
+ document.addEventListener("load", load);
 
 /*Look if the day and hour/minutes is equal to the given timeframe in the table*/
+
+function dateCheck(){
+if ((dagen !== "Zondag")&& (uren < '9') && (uren > '17:30')) {
+var getText;
+
+}}
+/*else 
+document.write("CLOSED");
+
+
+}*/
+
+
 
 /*If yes: OPEN*/
 /*If no: CLOSED*/
